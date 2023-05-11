10ceadb920da12139de2e7193ff2d536a28221a4
Abbos Mehmonov <100842718+Abbos6465@users.noreply.github.com>
2023-05-11 15:56:58 +0500

    Update permission

--- src/components/LogoHeader.vue
+++ src/components/LogoHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <RouterLink class="navbar-brand" :to="{name:'Home'}" >olma.uz</RouterLink>
+    <RouterLink :active="isActive" class="navbar-brand" :class="this.$route.name == 'Home' ? 'text-info fw-bolder' : 'text-white'" :to="{name:'Home'}" >olma.uz</RouterLink>
 </template>
 <script>
 export default {
